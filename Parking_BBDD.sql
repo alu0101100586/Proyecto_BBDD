e0e192cab16cb70b17aabae110399f22457cc61f
Jonay_Estévez_Díaz <72342547+alu0101100586@users.noreply.github.com>
2023-01-02 11:05:14 +0000

    Update Parking_BBDD.sql

--- Parking_BBDD.sql
+++ Parking_BBDD.sql
@@ -8,10 +8,41 @@
 -- Jonay Estévez Díaz          (alu0101100586)
 -- 
 
+--
+-- Creación de la base de datos
+--
 DROP DATABASE IF EXISTS parking_db;
-
 CREATE DATABASE parking_db WITH TEMPLATE = template0 ENCODING = 'UTF8';
-
 ALTER DATABASE parking_db OWNER TO postgres;
 
+--
+-- Realizamos la conexión a la base de datos
+-- 
 \connect parking_db
+
+--
+-- Creación del schema publico
+--
+CREATE SCHEMA public;
+AlTER SCHEMA public OWNER TO postgres;
+COMMENT On SCHEMA public IS 'standard public schema';
+
+--
+-- Creacion de la tabla X
+--
+
+--
+-- Creación de Disparadores
+--
+
+--
+-- Insercion de datos en la tabla X
+-- 
+
+--
+-- Creacion de las Aserciones
+--
+
+--
+-- Creacion de los Checks
+--
